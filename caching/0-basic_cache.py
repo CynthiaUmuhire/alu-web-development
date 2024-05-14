19c0a3c83ea7dc073ad6e0241195604315c12582
CynthiaUmuhire <c.umuhire@alustudent.com>
2024-05-14 10:50:40 +0200

    feat: bassic chaching

---
+++ caching/0-basic_cache.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+""" BaseCaching module
+"""
+
+
+class BaseCaching():
+    """ BaseCaching defines:
+      - constants of your caching system
+      - where your data are stored (in a dictionary)
+    """
+    MAX_ITEMS = 4
+
+    def __init__(self):
+        """ Initiliaze
+        """
+        self.cache_data = {}
+
+    def print_cache(self):
+        """ Print the cache
+        """
+        print("Current cache:")
+        for key in sorted(self.cache_data.keys()):
+            print("{}: {}".format(key, self.cache_data.get(key)))
+
+    def put(self, key, item):
+        """ check if the key or item is none
+        """
+        if key is None or item is None:
+            return
+        """ Add an item in the cache
+        """
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """ Check if an item or the key is none
+        """
+        if key is None or key is not self.cache_data:
+            return None
+        """ Get an item by key
+        """
+        return self.cache_data.get(key)
