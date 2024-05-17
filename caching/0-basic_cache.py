124302aea925489cc47ab43a7975e1705701d4cb
CynthiaUmuhire <c.umuhire@alustudent.com>
2024-05-17 15:58:55 +0200

    The caching techniques

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -1,41 +1,22 @@
 #!/usr/bin/python3
-""" BaseCaching module
+"""
+BasicCache class that inherits from BaseCaching
 """
 
+BaseCaching = __import__('base_caching').BaseCaching
 
-class BasicCache():
-    """ BaseCaching defines:
-      - constants of your caching system
-      - where your data are stored (in a dictionary)
-    """
-    MAX_ITEMS = 4
 
-    def __init__(self):
-        """ Initiliaze
-        """
-        self.cache_data = {}
-
-    def print_cache(self):
-        """ Print the cache
-        """
-        print("Current cache:")
-        for key in sorted(self.cache_data.keys()):
-            print("{}: {}".format(key, self.cache_data.get(key)))
+class BasicCache(BaseCaching):
+    """ Define BasicCache """
 
     def put(self, key, item):
-        """ check if the key or item is none
-        """
+        """ Add key/value pair to the cache """
         if key is None or item is None:
             return
-        """ Add an item in the cache
-        """
         self.cache_data[key] = item
 
     def get(self, key):
-        """ Check if an item or the key is none
-        """
-        if key is None or key is not self.cache_data:
+        """ Retrieve value from the cache based on given key """
+        if key is None or key not in self.cache_data:
             return None
-        """ Get an item by key
-        """
         return self.cache_data.get(key)
